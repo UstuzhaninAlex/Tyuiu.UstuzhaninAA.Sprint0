48d4282bb29e528832ff06c184e90cdf63866971
Алексей Устюжанин <aa.ustuzhanin@gmail.com>
2026-09-13 00:36:34 +0500

    Task2

---
+++ Tyuiu.UstuzhaninAA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.UstuzhaninAA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.UstuzhaninAA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Алексей";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Алексей", res);
+        }
+    }
+}
---
+++ Tyuiu.UstuzhaninAA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,6 @@
+﻿using Tyuiu.UstuzhaninAA.Sprint0.Task2.V0.Lib;
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(DataService.GetMessage("Алексей"));
+Console.ReadKey();
+
