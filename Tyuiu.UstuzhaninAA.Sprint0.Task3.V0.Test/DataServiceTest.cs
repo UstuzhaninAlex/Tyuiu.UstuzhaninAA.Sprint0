aec592bbc1a958d7c754d6e3ddcd970ef79d7ffe
Алексей Устюжанин <aa.ustuzhanin@gmail.com>
2026-09-13 21:19:22 +0500

    Task3

---
+++ Tyuiu.UstuzhaninAA.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿
+using Tyuiu.UstuzhaninAA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.UstuzhaninAA.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheakedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.UstuzhaninAA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.UstuzhaninAA.Sprint0.Task3.V0.Lib;
+
+
+Console.WriteLine(DataService.Sum(7, 7));
+Console.ReadKey();
